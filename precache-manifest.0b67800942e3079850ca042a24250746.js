a0fcaeeb5d98a38993291cfde828530337283d87
Sarah Ali <sarah23398@yahoo.ca>
2021-06-23 23:58:26 -0400

    Updates

---
+++ precache-manifest.0b67800942e3079850ca042a24250746.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50c3c08117acb3aa1f522816fe5109aa",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "03e46538e133102e5cf3",
+    "url": "/portfolio-website/static/css/main.9cfa8091.chunk.css"
+  },
+  {
+    "revision": "3adcfbfc05694ceef5c0",
+    "url": "/portfolio-website/static/js/2.59f33d89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-website/static/js/2.59f33d89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03e46538e133102e5cf3",
+    "url": "/portfolio-website/static/js/main.71101590.chunk.js"
+  },
+  {
+    "revision": "a77bb18274229563e654",
+    "url": "/portfolio-website/static/js/runtime-main.5816c056.js"
+  },
+  {
+    "revision": "a3e0b5f427803a187c1b62c5919196aa",
+    "url": "/portfolio-website/static/media/Poppins-Bold.a3e0b5f4.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/portfolio-website/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/portfolio-website/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "448a7d09cc503dd49effdb00781c33ca",
+    "url": "/portfolio-website/static/media/gitlab.448a7d09.svg"
+  },
+  {
+    "revision": "3e2de65d996d6f41572978799a8aa52b",
+    "url": "/portfolio-website/static/media/linkedin.3e2de65d.svg"
+  },
+  {
+    "revision": "28425530b1139c9d68c66d1d35228edd",
+    "url": "/portfolio-website/static/media/sarah.28425530.jpg"
+  }
+]);
